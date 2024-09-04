9008e2e31a22cd3eb6139832ea60741a9b4018d1
gudals9677 <gudals9677@gmail.com>
2024-09-05 00:13:42 +0900

    bean

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,63 @@
+package com.in28minutes.learn_spring_framework.helloworld;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+record Person (String name, int age, Address adress) {};
+
+record Address (String city, String code) {};
+
+@Configuration
+public class HelloWorldConfiguration {
+
+	@Bean
+	public String name() {
+		return "Ranga";
+	}
+	
+	@Bean
+	public int age() {
+		return 20;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 22, new Address("Changwon", "333"));
+	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) {
+		return new Person(name, age, address3);
+	}
+	
+	@Bean
+	@Primary
+	public Person person4Parameters(String name, int age, Address address) {
+		return new Person(name, age, address);
+	}
+	
+	@Bean
+	public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address) {
+		return new Person(name, age, address);
+	}
+	
+	@Bean(name = "address2")
+	@Primary
+	public Address address() {
+		return new Address("Seoul", "777");
+	}
+	
+	@Bean(name = "address3")
+	@Qualifier("address3qualifier")
+	public Address address3() {
+		return new Address("Monaco	", "555");
+	}
+}
+ 

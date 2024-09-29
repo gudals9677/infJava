23c489c87867419d7e00b1b4226d01f5127a40bf
gudals9677 <gudals9677@gmail.com>
2024-09-29 14:33:35 +0900

    restApi test commit

---
+++ restful-web-services/src/main/java/com/study/restapi/restful_web_services/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.study.restapi.restful_web_services.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}

086e48119bc601643a0bdb35d9729203ce1283a9
gudals9677 <gudals9677@gmail.com>
2024-10-02 22:25:58 +0900

    restApi

--- restful-web-services/src/main/java/com/study/restapi/restful_web_services/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/study/restapi/restful_web_services/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.study.restapi.restful_web_services.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,15 @@ public class HelloWorldController {
 	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping("/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean("Hello World " + name);
 	}
 }

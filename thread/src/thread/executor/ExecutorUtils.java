aa7ce66338f11efa85489bf31f2f4e8faf1e7770
gudals9677 <gudals9677@gmail.com>
2024-11-23 22:30:23 +0900

    es future

---
+++ thread/src/thread/executor/ExecutorUtils.java
@@ -0,0 +1,22 @@
+package thread.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static thread.util.MyLogger.log;
+
+public abstract class ExecutorUtils {
+
+    public static void printState(ExecutorService executorService) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int pool = poolExecutor.getPoolSize(); // 풀에서 관리되고있는 쓰레드 풀 갯수
+            int active = poolExecutor.getActiveCount(); // 작동중인 쓰레드 풀 갯수
+            int queuedTask = poolExecutor.getQueue().size(); // 큐에 대기중인 풀 갯수
+            long completedTask = poolExecutor.getCompletedTaskCount(); // 완료 된 풀 갯수
+
+            log("[pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTask + ", completedTask=" + completedTask + " ]");
+        } else {
+            log(executorService);
+        }
+    }
+}
